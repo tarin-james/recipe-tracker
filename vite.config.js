4bb417fc7aa02a5d62da86a912c93667fa313f4f
Tarin James <122317277+tarin-james@users.noreply.github.com>
2024-05-25 23:04:47 -0600

    file structure

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        create: resolve(__dirname, "src/create/index.html"),
+        search: resolve(__dirname, "src/search/index.html")
+      },
+    },
+  },
+});
